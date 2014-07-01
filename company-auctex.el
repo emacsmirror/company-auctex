ff5867829439f60e8ac53598f46c37941d532a92
Alexey Romanov <alexey.v.romanov@huawei.com>
2014-07-01 10:12:54 +0400

    Untabified

--- company-auctex.el
+++ company-auctex.el
@@ -73,25 +73,25 @@
 
 (defun company-auctex-expand-arg-info (arg-info)
   (loop for item in arg-info
-	append (cond
-		((or (stringp item) (and (vectorp item) (stringp (elt item 0))))
-		 (list item))
-		((vectorp item)
-		 (loop for item-2 in (or (assoc-default (or (car-safe (elt item 0)) (elt item 0))
-							company-auctex-arg-lookup-table 'equal) '(""))
-		       collect [item-2]))
-		(t
-		 (or (assoc-default (or (car-safe item) item) company-auctex-arg-lookup-table) '(""))))))
+        append (cond
+                ((or (stringp item) (and (vectorp item) (stringp (elt item 0))))
+                 (list item))
+                ((vectorp item)
+                 (loop for item-2 in (or (assoc-default (or (car-safe (elt item 0)) (elt item 0))
+                                                        company-auctex-arg-lookup-table 'equal) '(""))
+                       collect [item-2]))
+                (t
+                 (or (assoc-default (or (car-safe item) item) company-auctex-arg-lookup-table) '(""))))))
 
 (defun company-auctex-snippet-arg (n arg)
   (let* ((opt (vectorp arg))
-	 (item (if opt (elt arg 0) arg))
-	 (m (if (vectorp arg) (1+ n) n))
-	 (var (format "${%s}" item)))
+         (item (if opt (elt arg 0) arg))
+         (m (if (vectorp arg) (1+ n) n))
+         (var (format "${%s}" item)))
     (list (1+ m)
-	  (if opt
-	      (concat (format "${[") var "]}")
-	    (concat "{" var "}")))))
+          (if opt
+              (concat (format "${[") var "]}")
+            (concat "{" var "}")))))
 
 ;; Macros
 ;;
@@ -102,16 +102,16 @@
 (defun company-auctex-macro-snippet (arg-info)
   (let ((count 1))
     (apply 'concat (loop for item in (company-auctex-expand-arg-info arg-info)
-			 collect (destructuring-bind (n val)
-				     (company-auctex-snippet-arg count item)
-				   (setq count n)
-				   val)))))
+                         collect (destructuring-bind (n val)
+                                     (company-auctex-snippet-arg count item)
+                                   (setq count n)
+                                   val)))))
 
 (defun company-auctex-macro-candidates ()
    (let ((comlist (if TeX-symbol-list
-		      (mapcar (lambda (item)
-			        (or (car-safe (car item)) (car item)))
-			    TeX-symbol-list))))
+                      (mapcar (lambda (item)
+                                (or (car-safe (car item)) (car item)))
+                            TeX-symbol-list))))
     (all-completions ac-prefix comlist)))
 
 (defun company-auctex-macro-action ()
@@ -136,8 +136,8 @@
   (delete-region (1- (match-beginning 0)) (match-end 0))
   (if (texmathp)
       (progn
-	(insert "\\" candidate)
-	(yas/expand-snippet (company-auctex-macro-snippet (assoc-default candidate TeX-symbol-list))))
+        (insert "\\" candidate)
+        (yas/expand-snippet (company-auctex-macro-snippet (assoc-default candidate TeX-symbol-list))))
     (insert "$\\" candidate "$")
     (backward-char)
     (yas/expand-snippet (company-auctex-macro-snippet (assoc-default candidate TeX-symbol-list)))))
@@ -167,7 +167,7 @@
 
 (defun company-auctex-environment-candidates ()
   (let ((envlist (mapcar (lambda (item) (concat company-auctex-environment-prefix (car item)))
-			 LaTeX-environment-list)))
+                         LaTeX-environment-list)))
     (all-completions ac-prefix envlist)))
 
 (defun company-auctex-environment-action ()
@@ -175,9 +175,9 @@
   (delete-region (1- (match-beginning 0)) (match-end 0))
   (let ((candidate (substring candidate (length company-auctex-environment-prefix))))
     (yas/expand-snippet (format "\\begin{%s}%s\n$0\n\\end{%s}"
-				candidate
-				(company-auctex-macro-snippet (assoc-default candidate LaTeX-environment-list))
-				candidate))))
+                                candidate
+                                (company-auctex-macro-snippet (assoc-default candidate LaTeX-environment-list))
+                                candidate))))
 
 (ac-define-source auctex-environments
   '((init . LaTeX-environment-list)
@@ -215,11 +215,11 @@
     (requires . 0)
     (symbol . "b")
     (prefix . ,(concat "\\\\cite"
-		       "\\(?:"
-		         "\\[[^]]*\\]"
-		       "\\)?"
-		       "{\\([^},]*\\)"
-		       "\\="))))
+                       "\\(?:"
+                         "\\[[^]]*\\]"
+                       "\\)?"
+                       "{\\([^},]*\\)"
+                       "\\="))))
 
 ;; Setup
 ;;
@@ -229,9 +229,9 @@
   (setq ac-sources (append
                       '(ac-source-auctex-symbols
                         ac-source-auctex-macros
-			ac-source-auctex-environments
-			ac-source-auctex-labels
-			ac-source-auctex-bibs)
+                        ac-source-auctex-environments
+                        ac-source-auctex-labels
+                        ac-source-auctex-bibs)
                       ac-sources)))
 
 (add-hook 'LaTeX-mode-hook 'company-auctex-setup)
